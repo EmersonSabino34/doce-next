828f9ee6022c68e8ec2b1cf3381611fa8c420204
emerson sabino <emersonsabino34@gmail.com>
2024-12-23 21:08:04 +0000

    atualizacoes-de-imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="bg-black h-screen">
       <h1 className="bg-green-600">
+        <img src="./image/logo.jpg" width={400} alt="" />
+
         Emerson-Sabino
       </h1>
     </div>
